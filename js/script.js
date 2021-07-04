9a49b4489672268faa8a22a7ec596e46524eb622
Wojciech K <wojtekkk@protonmail.com>
2021-07-04 19:43:47 +0200

    welcome, toggleBackground and init functions added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-console.log("It's good to have you here")
+{
+    const welcome = () => {
+        console.log("It's good to have you here");
+    }
 
-let headerButtonTheme = document.querySelector(".js-headerButtonTheme");
-let body = document.querySelector(".js-body");
-let headerTheme = document.querySelector(".js-headerTheme");
+    welcome();
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const headerTheme = document.querySelector(".js-headerTheme");
+
+        body.classList.toggle("body--dark");
+        headerTheme.innerText = body.classList.contains("body--dark") ? "JASNY MOTYW" : "CIEMNY MOTYW";
+
+    }
+
+    const init = () => {
+        const headerButtonTheme = document.querySelector(".js-headerButtonTheme");
+        headerButtonTheme.addEventListener("click", toggleBackground);
+    }
+
+    init();
+
 let headerShow = document.querySelector(".js-headerShow");
 let headerName = document.querySelector(".js-headerName");
-
-headerButtonTheme.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-
-    headerTheme.innerText = body.classList.contains("body--dark") ? "JASNY MOTYW" : "CIEMNY MOTYW";
-});
 
 headerShow.addEventListener("click", () => {
     if(headerName.innerText === "...") {
@@ -22,3 +34,5 @@ headerShow.addEventListener("click", () => {
         headerName.innerText = "...";
     }
 });
+
+}
